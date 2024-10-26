55ba1014ccebf48eb3d8ed36f84a0f594f16d436
Alan Mong <alanmongx@gmail.com>
2024-10-25 17:26:52 -0700

    creating AppUser entity

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be675c6b0a7356d9bbed9d5f2e66e4eeddd65a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61b881ccf18d5d08cb11a2b7f8c29b9d6f48dfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
