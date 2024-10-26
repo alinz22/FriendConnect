edcea831ebce4612d2ab1d379426efa2e615c7a9
Alan Mong <alanmongx@gmail.com>
2024-10-26 02:21:39 -0700

    create userController with 2 read endpoints

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c44eb932ff9c81c983b0b26d62e3acb79e4b557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e8d53f4083ff3872b7ad532c717e6427d13b236")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
