da9d798ac1640ad279c8755af25751a339b8e5b0
Alan Mong <alanmongx@gmail.com>
2024-11-13 23:59:16 -0800

    token service

---
+++ API/Sevices/TokenService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API.Sevices;
+
+public class TokenService : ITokenService 
+{
+
+}
+
+public interface ITokenService
+{
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca1a0fe0454383d6fbd6d65069f7ced4d7b0147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67bf4a67f03e571acca51c622381dbf3fa3c248f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
