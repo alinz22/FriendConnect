959739231ba323ca79c38a2bce0ed75893a3bfad
Alan Mong <alanmongx@gmail.com>
2024-11-14 23:59:11 -0800

    add nuget identiy model

--- API/Sevices/TokenService.cs
+++ API/Sevices/TokenService.cs
@@ -1,9 +1,19 @@
 using System;
+using API.Entities;
 
 namespace API.Sevices;
 
-public class TokenService : ITokenService 
+public class TokenService(IConfiguration config) : ITokenService 
 {
+
+    public string CreateToken(AppUser user)
+    {
+        // Implement logic to generate JWT token
+        // For demonstration purposes, let's use a simple string for the token
+        var tokenKey = config["TokenKeyt"] ?? throw new Exception("No token key from app settings");
+        if (tokenKey.Length < 64) throw new Exception("Your key must be at least 64 characters");
+        var key
+    }
 
 }
 
